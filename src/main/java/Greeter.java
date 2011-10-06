0a85f7c1e98bbe82dd029b71d0c591a1c573667d
Lucas Holmquist <lholmquist@mlmic.com>
2011-10-06 11:53:10 -0400

    First Commit of Project

---
+++ src/main/java/Greeter.java
@@ -0,0 +1,23 @@
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lholmquist
+ * Date: 9/28/11
+ * Time: 11:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Named
+@RequestScoped
+public class Greeter {
+
+    public String getMessage()
+    {
+        return "Hello,  Person - experime   nt blah";
+    }
+
+
+
+
+}
